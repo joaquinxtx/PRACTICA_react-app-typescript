dc613a75fadbd7cdfdc0bae84ab85ec90d07ddf9
joaquinxtx <thejotex1998@gmail.com>
2022-09-09 14:32:34 -0300

    Control props alternativo

--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -9,38 +9,20 @@ export const useShoppingCart = () => {
       console.log({count});
       
       setShoppingCart(oldShoppingCart=>{
+        if(count ===0){
   
-        const productInCart:ProductInCart = oldShoppingCart[product.id] || {...product,count:0}
-  
-        if(Math.max(productInCart.count +count,0)>0){
-          productInCart.count += count;
-          return{
-            ...oldShoppingCart,
-            [product.id]:productInCart
-          }
-        }
           const{[product.id]:toDelete,...rest}=oldShoppingCart;
           console.log({toDelete})
   
           return{
             ...rest
           }
+        }
   
-        //                       CODIGO MAS LINDO
-        // if(count ===0){
-  
-        //   const{[product.id]:toDelete,...rest}=oldShoppingCart;
-        //   console.log({toDelete})
-  
-        //   return{
-        //     ...rest
-        //   }
-        // }
-  
-        // return {
-        //   ...oldShoppingCart,
-        //   [product.id]:{...product,count}
-        // }
+        return {
+          ...oldShoppingCart,
+          [product.id]:{...product,count}
+        }
       })
       
     }
